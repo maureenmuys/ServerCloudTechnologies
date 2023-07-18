a83082f8d43d51b3489fe10d6480a634bdd9084d
maureenmuys <maureen.muys@gmail.com>
2023-07-18 11:11:54 +0200

    Again

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES_USER}:{POSTGRES
 # postgresql://username:password@host:port/database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hello_flask:hello_flask@db:5432/hello_flask_dev'
 
-from models import db, CustomerFavs
+from models import db, UserFavs
 
 db.init_app(app)
 with app.app_context():
@@ -35,70 +35,70 @@ def main():
 
 @app.route("/save", methods=['POST'])
 def save():
-    customer = str(request.form['customer']).lower()
+    username = str(request.form['username']).lower()
     place = str(request.form['place']).lower()
     food = str(request.form['food']).lower()
 
-    # check if data of the customer already exists in the redis
-    if red.hgetall(customer).keys():
-        print("hget customer:", red.hgetall(customer))
-        # return a msg to the template, saying the customer already exists(from redis)
-        return render_template('index.html', customer_exists=1, msg='(From Redis)', customer=customer, place=red.hget(customer,"place").decode('utf-8'), food=red.hget(customer,"food").decode('utf-8'))
+    # check if data of the username already exists in the redis
+    if red.hgetall(username).keys():
+        print("hget username:", red.hgetall(username))
+        # return a msg to the template, saying the user already exists(from redis)
+        return render_template('index.html', user_exists=1, msg='(From Redis)', username=username, place=red.hget(username,"place").decode('utf-8'), food=red.hget(username,"food").decode('utf-8'))
 
     # if not in redis, then check in db
-    elif len(list(red.hgetall(customer)))==0:
-        record =  CustomerFavs.query.filter_by(customer=customer).first()
+    elif len(list(red.hgetall(username)))==0:
+        record =  UserFavs.query.filter_by(username=username).first()
         print("Records fecthed from db:", record)
         
         if record:
-            red.hset(customer, "place", place)
-            red.hset(customer, "food", food)
-            # return a msg to the template, saying the customer already exists(from database)
-            return render_template('index.html', customer_exists=1, msg='(From DataBase)', customer=customer, place=record.place, food=record.food)
+            red.hset(username, "place", place)
+            red.hset(username, "food", food)
+            # return a msg to the template, saying the user already exists(from database)
+            return render_template('index.html', user_exists=1, msg='(From DataBase)', username=username, place=record.place, food=record.food)
 
-    # if data of the customer doesnot exist anywhere, create a new record in DataBase and store in Redis also
+    # if data of the username doesnot exist anywhere, create a new record in DataBase and store in Redis also
     # create a new record in DataBase
-    new_record = CustomerFavs(customer=customer, place=place, food=food)
+    new_record = UserFavs(username=username, place=place, food=food)
     db.session.add(new_record)
     db.session.commit()
 
     # store in Redis also
-    red.hset(customer, "place", place)
-    red.hset(customer, "food", food)
+    red.hset(username, "place", place)
+    red.hset(username, "food", food)
 
     # cross-checking if the record insertion was successful into database
-    record =  CustomerFavs.query.filter_by(customer=customer).first()
+    record =  UserFavs.query.filter_by(username=username).first()
     print("Records fetched from db after insert:", record)
 
     # cross-checking if the insertion was successful into redis
-    print("key-values from redis after insert:", red.hgetall(customer))
+    print("key-values from redis after insert:", red.hgetall(username))
 
     # return a success message upon saving
-    return render_template('index.html', saved=1, customer=customer, place=red.hget(customer, "place").decode('utf-8'), food=red.hget(customer, "food").decode('utf-8'))
+    return render_template('index.html', saved=1, username=username, place=red.hget(username, "place").decode('utf-8'), food=red.hget(username, "food").decode('utf-8'))
 
 @app.route("/keys", methods=['GET'])
 def keys():
-	records = CustomerFavs.query.all()
+	records = UserFavs.query.all()
 	names = []
 	for record in records:
-		names.append(record.customer)
-	return render_template('index.html', keys=1, customers=names)
+		names.append(record.username)
+	return render_template('index.html', keys=1, usernames=names)
 
 
 @app.route("/get", methods=['POST'])
 def get():
-	customer = request.form['customer']
-	print("customer:", customer)
-	customer_data = red.hgetall(customer)
-	print("GET Redis:", customer_data)
+	username = request.form['username']
+	print("Username:", username)
+	user_data = red.hgetall(username)
+	print("GET Redis:", user_data)
 
-	if not customer_data:
-		record = CustomerFavs.query.filter_by(customer=customer).first()
+	if not user_data:
+		record = UserFavs.query.filter_by(username=username).first()
 		print("GET Record:", record)
 		if not record:
 			print("No data in redis or db")
-			return render_template('index.html', no_record=1, msg=f"Record not yet defined for {customer}")
-		red.hset(customer, "place", record.place)
-		red.hset(customer, "food", record.food)
-		return render_template('index.html', get=1, msg="(From DataBase)",customer=customer, place=record.place, food=record.food)
-	return render_template('index.html',get=1, msg="(From Redis)", customer=customer, place=customer_data[b'place'].decode('utf-8'), food=customer_data[b'food'].decode('utf-8'))
+			return render_template('index.html', no_record=1, msg=f"Record not yet defined for {username}")
+		red.hset(username, "place", record.place)
+		red.hset(username, "food", record.food)
+		return render_template('index.html', get=1, msg="(From DataBase)",username=username, place=record.place, food=record.food)
+	return render_template('index.html',get=1, msg="(From Redis)", username=username, place=user_data[b'place'].decode('utf-8'), food=user_data[b'food'].decode('utf-8'))
--- app/models.py
+++ app/models.py
@@ -2,16 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class CustomerFavs(db.Model):
-	customer = db.Column(db.String, primary_key=True)
+class UserFavs(db.Model):
+	username = db.Column(db.String, primary_key=True)
 	place = db.Column(db.String)
 	food = db.Column(db.String)
 
-	def __init__(self, customer, place, food):
-		self.customer=customer
+	def __init__(self, username, place, food):
+		self.username=username
 		self.place=place
 		self.food=food
 
 	def __repr__(self):
-		return f'<Customer-Place-Food : {self.customer}-{self.place}-{self.food}'
+		return f'<User-Place-Food : {self.username}-{self.place}-{self.food}'
 
